aaaccc550732d8113d6b86a9c243ff80b4b28cdd
Marc <marcp8363@gmail.com>
2021-04-01 13:27:13 +0200

    Primer

---
+++ src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/Projectem07uf2PalmaAliagaApplication.java
@@ -0,0 +1,13 @@
+package edu.fje.daw2.projectem07uf2palma_aliaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectem07uf2PalmaAliagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projectem07uf2PalmaAliagaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/fje/daw2/projectem07uf2palma_aliaga/Projectem07uf2PalmaAliagaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fje.daw2.projectem07uf2palma_aliaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectem07uf2PalmaAliagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
