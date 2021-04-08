7d060dc87253900b89d348b83af8ec8ef30bdcd0
sealja2021daw2 <sealja2021daw2@gmail.com>
2021-04-08 19:32:25 +0200

    implementacio del servei jugador i connexio bd mysql

--- src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/model/Jugador.java
+++ src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/model/Jugador.java
@@ -1,11 +1,47 @@
 package edu.fje.daw2.projectem07uf2palma_aliaga.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class Jugador {
+public class Jugador implements Serializable {
+    @Column(length = 25)
     @Id String usuario;
-    private String contraseña;
-    private String puntuacio;
+    @Column(length = 25)
+    private String contrasena;
+    private int puntuacio=0;
 
+    //constructor
+    public Jugador() {}
+    public Jugador(String usuario, String contrasena) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    //getters i setters
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getPuntuacio() {
+        return puntuacio;
+    }
+
+    public void setPuntuacio(int puntuacio) {
+        this.puntuacio = puntuacio;
+    }
+    
 }
---
+++ src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/service/JugadorService.java
@@ -0,0 +1,32 @@
+package edu.fje.daw2.projectem07uf2palma_aliaga.service;
+
+import edu.fje.daw2.projectem07uf2palma_aliaga.model.Jugador;
+import edu.fje.daw2.projectem07uf2palma_aliaga.repository.JugadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JugadorService {
+    private  JugadorRepository ju;
+
+    //llistar jugadors
+    public List<Jugador> findAll() {
+        var it = ju.findAll();
+        var Jugadors = new ArrayList<Jugador>();
+        it.forEach(e -> Jugadors.add(e));
+        return Jugadors;
+    }
+    //eliminar jugador
+    public void deleteById(String JugadorUsername) {
+        ju.deleteById(JugadorUsername);
+    }
+
+    //
+    public void afegirUsuari(String username, String contrasena){
+        var p1 = new Jugador(username, contrasena);
+        ju.save(p1);
+    }
+
+}
