d8fedada378db8fafd651e601dc4a53b9f2c00d9
sealja2021daw2 <sealja2021daw2@gmail.com>
2021-04-08 17:43:04 +0200

    repositori jugador

--- src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/controller/JugadorController.java
+++ src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/controller/JugadorController.java
@@ -1,12 +1,25 @@
 package edu.fje.daw2.projectem07uf2palma_aliaga.controller;
 
-import edu.fje.daw2.projectem07uf2palma_aliaga.controller.JugadorController;
+import
+        edu.fje.daw2.projectem07uf2palma_aliaga.controller.JugadorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import edu.fje.daw2.projectem07uf2palma_aliaga.model.Jugador;
+import edu.fje.daw2.projectem07uf2palma_aliaga.repository.JugadorRepository;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class JugadorController {
+    Set<Jugador> jugadors = new HashSet<>();
 
+    @PostMapping("/afegirJugador")
+    public String afegirAlumne(@RequestParam String usuario,
+                               @RequestParam String contrasena,
+                               Model model){
+        return "hola";
+    }
 }
---
+++ src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/repository/JugadorRepository.java
@@ -0,0 +1,13 @@
+package edu.fje.daw2.projectem07uf2palma_aliaga.repository;
+
+
+import java.util.List;
+
+import edu.fje.daw2.projectem07uf2palma_aliaga.model.Jugador;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JugadorRepository extends CrudRepository<Jugador, Long>{
+}
