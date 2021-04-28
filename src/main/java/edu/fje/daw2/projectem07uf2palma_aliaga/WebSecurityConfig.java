5ea5b8ec8b5b8e0e71e0810fccef4fc0fc108f54
sealja2021daw2 <sealja2021daw2@gmail.com>
2021-04-28 23:16:14 +0200

    outh

---
+++ src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+
+package edu.fje.daw2.projectem07uf2palma_aliaga;
+
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/index.html", "/spring-logo.svg")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
