bc29fcb8acfc57cf24b6e7a7d446b1d322b78011
markituss16 <marcp8363@gmail.com>
2021-04-29 02:37:52 +0200

    acces

--- src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/repository/PartidaRepositori.java
+++ src/main/java/edu/fje/daw2/projectem07uf2palma_aliaga/repository/PartidaRepositori.java
@@ -1,7 +1,10 @@
-package edu.fje.daw2.projectem07uf2palma_aliaga;
+package edu.fje.daw2.projectem07uf2palma_aliaga.repository;
 
 import edu.fje.daw2.projectem07uf2palma_aliaga.model.Partida;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PartidaRepositori extends MongoRepository<Partida, String> {
+    Partida findByCodiPartida(int codiPartida);
 }
